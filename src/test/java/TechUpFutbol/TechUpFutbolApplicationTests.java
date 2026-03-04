0a23dc177a007fe8eb637996335fd6fc1257c4cd
VanessaTorresM <jeimmy.torres-m@mail.escuelaing.edu.co>
2026-03-03 19:34:32 -0500

    creacion de las carpetas

---
+++ src/test/java/TechUpFutbol/TechUpFutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package TechUpFutbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechUpFutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
